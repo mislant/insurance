8144fff81f40973c3c196c445b60d4506ca2176b
mislant <kiryakz2000@gmail.com>
2021-01-25 22:38:22 +0600

    alter subject

--- commands/database/04_subject.sql
+++ commands/database/04_subject.sql
@@ -1,8 +1,9 @@
 CREATE TABLE IF NOT EXISTS insurance.subject
 (
-    id             serial primary key,
-    name           varchar(60) not null,
-    surname        varchar(60) not null,
-    identification char(12)    not null,
-    experience     smallint default 0
+    id                serial primary key,
+    name              varchar(60) not null,
+    surname           varchar(60) not null,
+    identification    char(12)    not null,
+    experience        smallint default 0,
+    residence_country char(3)     not null
 );
